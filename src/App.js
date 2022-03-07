896e45b347b5d1fc971b709523a2d1799cb30f52
Mohammed ALWahidi <nvs.apple01@gmail.com>
2022-03-07 17:56:10 +0300

    add limt & pages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,85 @@
 import { useEffect, useState } from "react";
-import './index.css'
+import "./index.css";
 import PokemonCard from "./PokemonCard";
+import { TailSpin } from "react-loader-spinner";
+
 function App() {
-  const API_URL = ' https://pokeapi.co/api/v2/pokemon/?limit=151'
-  const [pokemons,setPokemons] = useState([])
-  const [searchInput,setSeachInput] = useState('')
-  const [filterdPokemon,setFilterdPokemon] = useState([])
+  const API_URL = " https://pokeapi.co/api/v2/pokemon/?offset=0&limit=12&count=50";
+  const [pokemons, setPokemons] = useState([]);
+  const [searchInput, setSeachInput] = useState("");
+  const [filterdPokemon, setFilterdPokemon] = useState([]);
+  const [prevPage, setPrevPage] = useState(null);
+  const [nextPage, setNextPage] = useState(null);
+  const [pageCounter, setPageCounter] = useState(1);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() =>{
-    const getPokmeonName = async () => {
-      const resp = await fetch(API_URL)
-      const data = await resp.json()
-      setPokemons(data.results.map(elem => elem.name))
-    }
-    getPokmeonName()
+  useEffect(() => {
+    const getPokmeonName = () => {
+      setLoading(true);
+      setTimeout(async () => {
+        const resp = await fetch(API_URL);
+        const data = await resp.json();
+        setPokemons(data.results.map((elem) => elem.name));
+        setNextPage(data.next);
+        setLoading(false);
+      }, 500);
+    };
+    getPokmeonName();
+  }, []);
 
-  },[])
+  const fetchNextPrevPokemon = (e) => {
+    setSeachInput("");
+    setLoading(true);
+    const clickedBtn = e.target.innerHTML;
+    setTimeout(async () => {
+      const resp = await fetch(clickedBtn === "NEXT" ? nextPage : prevPage);
+      const data = await resp.json();
+      setPokemons(data.results.map((elem) => elem.name));
+      setNextPage(data.next);
+      setPrevPage(data.previous);
+      setLoading(false);
+      setPageCounter((prev) => (clickedBtn === "NEXT" ? prev + 1 : prev - 1));
+    }, 500);
+  };
 
-  const setFilterPokemons = (e) =>{
-    setSeachInput(e.target.value)
-    const filteredPokeoms =  pokemons.filter(pokemon => pokemon.toLowerCase().includes(searchInput.toLocaleLowerCase()))
-    setFilterdPokemon(filteredPokeoms)
+  const setFilterPokemons = (e) => {
+    setSeachInput(e.target.value);
+    const filteredPokeoms = pokemons.filter((pokemon) =>
+      pokemon.toLowerCase().includes(searchInput.toLocaleLowerCase())
+    );
+    setFilterdPokemon(filteredPokeoms);
+  };
 
-  }
   return (
     <div>
-     <h1 className="title">Pokemon Card</h1>
-     <div className="search_pokemon">
-     <input type="search" onChange ={(e) => setFilterPokemons(e)} />
-     </div>
-     <div className="content">
-       {searchInput !== '' && filterdPokemon.map((pokemon,idx) => <PokemonCard  name={pokemon} key ={idx}  />)}
-        {searchInput === ''  && pokemons.map((pokemon,idx) => <PokemonCard  name={pokemon} key ={idx} />)}
-     </div>
+      <h1 className='title'>Pokemon Card</h1>
+      <div className='search_pokemon'>
+        <input type='search' value={searchInput || ""} onChange={(e) => setFilterPokemons(e)} />
+      </div>
+      <div className='content'>
+        {searchInput !== "" &&
+          filterdPokemon.map((pokemon, idx) => <PokemonCard name={pokemon} key={idx} TailSpin={TailSpin} />)}
+
+        {searchInput === "" && !loading && pokemons.map((pokemon, idx) => <PokemonCard name={pokemon} key={idx} />)}
+      </div>
+
+      {!loading && (
+        <div className='change-page-btn'>
+          {pageCounter > 1 && (
+            <button className='btn' onClick={(e) => fetchNextPrevPokemon(e)}>
+              PREV
+            </button>
+          )}
+          <button className='btn' onClick={(e) => fetchNextPrevPokemon(e)}>
+            NEXT
+          </button>
+        </div>
+      )}
+      {loading && (
+        <div className='loading'>
+          <TailSpin color='orangered' height={200} width={200} />
+        </div>
+      )}
     </div>
   );
 }
--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -1,29 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-function PokemonCard({name}) {
-    const [pokemonImage,setPokemonImage] = useState('')
-    useEffect(() =>{
-        const abortController = new AbortController();
-        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`,{signal:abortController.signal})
-        .then(resp => resp.json())
-        .then(data => setPokemonImage(data.sprites.other.dream_world.front_default))
-        .catch(err =>{
-            if(err.name === 'AbortError'){
-                console.log('fetch-aborted')
-                return
-            }
-        })
-        return () => abortController.abort();
-    },[name])
+function PokemonCard({ name }) {
+  const [pokemonImage, setPokemonImage] = useState("");
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, { signal: abortController.signal })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPokemonImage(data.sprites.other.dream_world.front_default);
+      })
+      .catch((err) => {
+        if (err === "AbortError") {
+          return;
+        } else {
+          console.log(err);
+        }
+      });
+    return () => abortController.abort();
+  }, [name]);
 
   return (
-    <div className="card">
-        <h3 className='pokemon_name'>{name}</h3>
-        <div className="image">
+    <div className='card'>
+      <h3 className='pokemon_name'>{name}</h3>
+      <div className='image'>
         <img src={pokemonImage} alt={pokemonImage} loading='lazy' />
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PokemonCard
+export default PokemonCard;
